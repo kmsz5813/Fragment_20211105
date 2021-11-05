22c0c37999dfc8278f20a8a27b2fb340c9a4c9af
김민성 <kmsz5813@naver.com>
2021-11-05 11:40:05 +0900

    두번째 프래그먼트 제작 (미배치)

---
+++ app/src/main/java/com/nepplus/fragment_20211105/fragments/SecondFragment.kt
@@ -0,0 +1,33 @@
+package com.nepplus.fragment_20211105.fragments
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.nepplus.fragment_20211105.R
+import kotlinx.android.synthetic.main.fragment_second.*
+
+class SecondFragment : Fragment()  {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_second, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        btnToast.setOnClickListener {
+
+            Toast.makeText(requireContext(), "프래그먼트 토스트", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+}
